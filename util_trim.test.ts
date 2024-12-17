70847de1b5342f9e29b1f8a1b7ff9cda916b39e2
devcui <devcui@outlook.com>
2024-12-17 17:55:39 +0800

    chore: chore code

--- util_trim.test.ts
+++ util_trim.test.ts
@@ -1,22 +1,22 @@
 import { assertEquals } from "@std/assert/equals";
-import { TrimLeft } from "./util_trim.ts";
+import { StringTrimLeft } from "./util_trim.ts";
 
-Deno.test('TrimLeft should remove leading characters specified in the cutset', () => {
-  assertEquals(TrimLeft('---hello---', '-'), 'hello---');
-  assertEquals(TrimLeft('###hello###', '#'), 'hello###');
-  assertEquals(TrimLeft('   hello   ', ' '), 'hello   ');
+Deno.test('StringTrimLeft should remove leading characters specified in the cutset', () => {
+  assertEquals(StringTrimLeft('---hello---', '-'), 'hello---');
+  assertEquals(StringTrimLeft('###hello###', '#'), 'hello###');
+  assertEquals(StringTrimLeft('   hello   ', ' '), 'hello   ');
 });
 
-Deno.test('TrimLeft should not remove characters not specified in the cutset', () => {
-  assertEquals(TrimLeft('---hello---', '#'), '---hello---');
-  assertEquals(TrimLeft('###hello###', '-'), '###hello###');
-  assertEquals(TrimLeft('   hello   ', '-'), '   hello   ');
+Deno.test('StringTrimLeft should not remove characters not specified in the cutset', () => {
+  assertEquals(StringTrimLeft('---hello---', '#'), '---hello---');
+  assertEquals(StringTrimLeft('###hello###', '-'), '###hello###');
+  assertEquals(StringTrimLeft('   hello   ', '-'), '   hello   ');
 });
 
-Deno.test('TrimLeft should return the original string if cutset is empty', () => {
-  assertEquals(TrimLeft('---hello---', ''), '---hello---');
+Deno.test('StringTrimLeft should return the original string if cutset is empty', () => {
+  assertEquals(StringTrimLeft('---hello---', ''), '---hello---');
 });
 
-Deno.test('TrimLeft should return an empty string if the input string is empty', () => {
-  assertEquals(TrimLeft('', '-'), '');
+Deno.test('StringTrimLeft should return an empty string if the input string is empty', () => {
+  assertEquals(StringTrimLeft('', '-'), '');
 });

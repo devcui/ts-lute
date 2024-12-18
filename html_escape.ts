4c078b65ebfb4e0410e062ec3b7d97ee967fc9fd
devcui <devcui@outlook.com>
2024-12-18 15:42:40 +0800

    feat: add html escape funcs

--- html_escape.ts
+++ html_escape.ts
@@ -2,6 +2,7 @@ import { Buffer } from "@std/io/buffer";
 import { Bytes } from "./types.ts";
 import { IndexAny } from "./util_index.ts";
 
+export const HtmlEscapedChars = "&'<>\"\r";
 export const HtmlReplacementTable: Array<string> = [
   "\u20AC", // First entry is what 0x80 should be replaced with.
   "\u0081",
@@ -39,33 +40,14 @@ export const HtmlReplacementTable: Array<string> = [
   // 0x0D->'\u000D' is a no-op
 ];
 
-// unescapeEntity reads an entity like "&lt;" from b[src:] and writes the
-// corresponding "<" to b[dst:], returning the incremented dst and src cursors.
-// Precondition: b[src] == '&' && dst <= src.
-// attribute should be true if parsing an attribute value.
-export function HtmlUnescapeEntity(
-  bytes: Bytes,
-  dst: number,
-  src: number,
-  attribute: boolean,
-): {
-  dst: number;
-  src: number;
-} {
-  // https://html.spec.whatwg.org/multipage/syntax.html#consume-a-character-reference
-
-  // i starts at 1 because we already know that s[0] == '&'.
-  const index = 1;
-  const newBytes = bytes.slice(src, bytes.length);
-  if (newBytes.length <= 1) {
-    bytes[dst] = bytes[src];
-    return { dst: dst + 1, src: src + 1 };
+export function HtmlEscapeString(s: string): string {
+  if (IndexAny(s, HtmlEscapedChars) === -1) {
+    return s;
   }
-
-  return { dst, src };
+  const buffer = new Buffer();
+  HtmlEscape(buffer, s);
+  return new TextDecoder().decode(buffer.bytes());
 }
-
-export const HtmlEscapedChars = "&'<>\"\r";
 
 export function HtmlEscape(buffer: Buffer, s: string): void {
   let i = IndexAny(s, HtmlEscapedChars);
@@ -104,4 +86,73 @@ export function HtmlEscape(buffer: Buffer, s: string): void {
   }
 
   buffer.write(encoder.encode(s));
+}
+
+export function HtmlUnescapeString(s: string): string {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "&") {
+      return decoder.decode(HtmlUnescape(encoder.encode(s), false));
+    }
+  }
+  return s;
+}
+
+export function HtmlUnescape(b: Bytes, attribute: boolean): Uint8Array {
+  for (let i = 0; i < b.length; i++) {
+    if (b[i] === "&".charCodeAt(0)) {
+      let { dst, src } = HtmlUnescapeEntity(b, i, i, attribute);
+      while (src < b.length) {
+        const c = b[src];
+        if (c === "&".charCodeAt(0)) {
+          const res = HtmlUnescapeEntity(b, dst, src, attribute);
+          dst = res.dst;
+          src = res.src;
+        } else {
+          b[dst] = c;
+          dst++;
+          src++;
+        }
+      }
+      return b.slice(0, dst);
+    }
+  }
+  return b;
+}
+
+// unescapeEntity reads an entity like "&lt;" from b[src:] and writes the
+// corresponding "<" to b[dst:], returning the incremented dst and src cursors.
+// Precondition: b[src] == '&' && dst <= src.
+// attribute should be true if parsing an attribute value.
+export function HtmlUnescapeEntity(
+  bytes: Bytes,
+  dst: number,
+  src: number,
+  attribute: boolean,
+): {
+  dst: number;
+  src: number;
+} {
+  // https://html.spec.whatwg.org/multipage/syntax.html#consume-a-character-reference
+
+  // i starts at 1 because we already know that s[0] == '&'.
+  const index = 1;
+  const newBytes = bytes.slice(src, bytes.length);
+  if (newBytes.length <= 1) {
+    bytes[dst] = bytes[src];
+    return { dst: dst + 1, src: src + 1 };
+  }
+
+  return { dst, src };
+}
+
+export function HtmlLower(b: Bytes): Uint8Array {
+  for (let i = 0; i < b.length; i++) {
+    const c = b[i];
+    if ("A".charCodeAt(0) <= c && c <= "Z".charCodeAt(0)) {
+      b[i] = c + "a".charCodeAt(0) - "A".charCodeAt(0);
+    }
+  }
+  return b;
 }
